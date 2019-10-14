666926c1256cc6b47de0783ecc3c6638a96419b3
Albert <albert.nico142@gmail.com>
2019-10-14 18:33:38 +0300

    hola amigos

--- Trial.py
+++ Trial.py
@@ -5,7 +5,7 @@ import time
 pygame.init()
 
 windows_width = 1000
-windows_height = 50
+windows_height = 500
 
 win = pygame.display.set_mode((windows_width, windows_height))
 pygame.display.set_caption("First Game")
